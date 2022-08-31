a26d25feaae5510c7fe23b646f0c891b56c26bb6
karthik dasari <karthik.dasari2001@gmail.com>
2022-08-31 19:14:24 +0530

    update docker files

--- pm/pm/settings.py
+++ pm/pm/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'db_passmanager',
-        'HOST': '127.0.0.1',
+        'HOST': 'db',
         'PORT': '3306',
         'USER': 'root',
         'PASSWORD': 'lokesh',
